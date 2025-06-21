7321cae8ed6f3ed94e086cb218d2422ff0c59228
Yuto-design <c1320527@st.kanazawa-it.ac.jp>
2025-06-21 17:42:32 +0900

    Second Commit

--- public/index.php
+++ public/index.php
@@ -1,2 +1,35 @@
-<?php
-    phpinfo();
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex" />
+    <title>Car Introduction</title>
+    <link rel="stylesheet" href="./assets/main.css">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <a href="index.php">car introduction</a>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>List of Brands</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Main -->
+    <div class="main-visual">
+        <div>
+            <h1>Car Introduction Site</h1>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <p>&copy 2025 tech y.s</p>
+    </footer>
+</body>
+</html>
